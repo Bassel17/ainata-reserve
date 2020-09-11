7a30a0566c2fe0ad2072039072631f541ed9a7fc
Bassel <basselkanso82@gmail.com>
2020-09-11 17:04:31 +0300

    adding footer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 import ScreenSwitcher from './Components/ScreenSwitcher';
 import HomeScreen from './Components/Screens/homeScreen/homeScreen';
+import Footer from './Components/Screens/footer/Footer';
 
 function App() {
   return (
-   <ScreenSwitcher content={[HomeScreen]}/>
+   <ScreenSwitcher content={[HomeScreen,Footer]}/>
     
   );
 }
---
+++ src/Components/Screens/footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () =>{
+
+    return(
+        <div className="footer">
+            <div className="footer__info">
+                <div className = "footer__info__details"></div>
+                <div className = "footer__info__images"></div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Footer;
