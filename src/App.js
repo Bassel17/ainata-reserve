573d67f54ed3d50fe446dbc69c7247c9cbae8be5
Bassel <basselkanso82@gmail.com>
2020-09-15 21:13:31 +0300

    feat: made second screen responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-// import ScreenSwitcher from './Components/ScreenSwitcher';
-// import HomeScreen from './Components/Screens/homeScreen/homeScreen';
-// import Footer from './Components/Screens/footer/Footer';
-// import TerriorScreen from './Components/Screens/terroirScreen/TerroirScreen';
-// import secondScreen from './Components/Screens/secondScreen/secondScreen';
-// import PassionMadeVisible from './Components/Screens/PassionMadeVisible/passion';
+import ScreenSwitcher from './Components/ScreenSwitcher';
+import HomeScreen from './Components/Screens/homeScreen/homeScreen';
+import Footer from './Components/Screens/footer/Footer';
+import TerriorScreen from './Components/Screens/terroirScreen/TerroirScreen';
+import SecondScreen from './Components/Screens/secondScreen/secondScreen';
+import PassionMadeVisible from './Components/Screens/PassionMadeVisible/passion';
 import MeetTheCharacter from './Components/Screens/MeetTheCharacter/meet';
 import {
   BrowserRouter as Router,
@@ -14,8 +14,7 @@ import {
 function App() {
   return (
   <Router>
-     {/* <ScreenSwitcher content={[HomeScreen,PassionMadeVisible,MeetTheCharacter,TerriorScreen,Footer]}/> */}
-      <MeetTheCharacter/>
+     <ScreenSwitcher content={[HomeScreen,SecondScreen,PassionMadeVisible,MeetTheCharacter,TerriorScreen,Footer]}/>
   </Router>
   );
 }
