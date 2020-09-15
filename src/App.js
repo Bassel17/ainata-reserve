d799106205d7b12a03346ab6d1697982fcaadb87
Bassel <basselkanso82@gmail.com>
2020-09-15 09:52:06 +0300

    make terrior screen

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import ScreenSwitcher from './Components/ScreenSwitcher';
 import HomeScreen from './Components/Screens/homeScreen/homeScreen';
 import Footer from './Components/Screens/footer/Footer';
+import TerriorScreen from './Components/Screens/terroirScreen/TerroirScreen';
 
 function App() {
   return (
-   <ScreenSwitcher content={[HomeScreen,Footer]}/>
-    
+  //  <ScreenSwitcher content={[HomeScreen,Footer]}/>
+    <TerriorScreen/>
   );
 }
 
---
+++ src/Components/Screens/terroirScreen/TerroirScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './TerroirScreen.css';
+
+
+const TerroirScreen = () => {
+
+    return(
+        <div className="terrior">
+            <div className="terrior__image"></div>
+            <div className="terrior__text">
+                <h1>
+                A COMPLEX <br/>EXPLOSION<br/>
+                FOR THE SENSES
+                </h1>
+                <div>DISCOVER OUR TERROIR</div>
+            </div>
+        </div>
+    );
+
+}
+
+export default TerroirScreen;
