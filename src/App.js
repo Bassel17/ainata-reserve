f6b0aba6c64f0b8bcb52bf33faf557278a6d1a39
Bassel <basselkanso82@gmail.com>
2020-09-25 18:31:29 +0300

    make animation work on all screens

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, {useState} from 'react';
 import './App.css';
-// import ScreenSwitcher from './Components/ScreenSwitcher';
-// import HomeScreen from './Components/Screens/homeScreen/homeScreen';
-// import Footer from './Components/Screens/footer/Footer';
-// import TerriorScreen from './Components/Screens/terroirScreen/TerroirScreen';
-// import SecondScreen from './Components/Screens/secondScreen/secondScreen';
-// import PassionMadeVisible from './Components/Screens/PassionMadeVisible/passion';
-// import MeetTheCharacter from './Components/Screens/MeetTheCharacter/meet';
-// import Generosity from './Components/Screens/Generosity/generosity'
-// import Elegance from './Components/Screens/Elegance/elegance';
-// import MeetTheFamily from './Components/Screens/MeetTheFamily/meetTheFamily';
-// import FollowTheFox from './Components/Screens/FollowTheFox/FollowTheFox';
-// import NavigationMenu from './Components/navigationMenu/NavigationMenu';
+import ScreenSwitcher from './Components/ScreenSwitcher';
+import HomeScreen from './Components/Screens/homeScreen/homeScreen';
+import Footer from './Components/Screens/footer/Footer';
+import TerriorScreen from './Components/Screens/terroirScreen/TerroirScreen';
+import SecondScreen from './Components/Screens/secondScreen/secondScreen';
+import PassionMadeVisible from './Components/Screens/PassionMadeVisible/passion';
+import MeetTheCharacter from './Components/Screens/MeetTheCharacter/meet';
+import Generosity from './Components/Screens/Generosity/generosity'
+import Elegance from './Components/Screens/Elegance/elegance';
+import MeetTheFamily from './Components/Screens/MeetTheFamily/meetTheFamily';
+import FollowTheFox from './Components/Screens/FollowTheFox/FollowTheFox';
+import NavigationMenu from './Components/navigationMenu/NavigationMenu';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
-//import Ainata12 from './Components/Wines-Pages/WineScreens/Ainata12/ainata12';
+// import Ainata12 from './Components/Wines-Pages/WineScreens/Ainata12/ainata12';
 //import Ainata24 from './Components/Wines-Pages/WineScreens/Ainata24/ainata24';
 //import Syrah from './Components/Wines-Pages/WineScreens/Syrah/syrah';
-import Chardonnay from './Components/Wines-Pages/WineScreens/Chardonnay/chardonnay';
+// import Chardonnay from './Components/Wines-Pages/WineScreens/Chardonnay/chardonnay';
 //import TheWines from './Components/Wines-Pages/WineScreens/theWines/theWines';
 //import Ainata12 from './Components/Wines-Pages/WineScreens/Ainata12/ainata12';
 
@@ -38,7 +38,7 @@ function App() {
     {/* <NavigationMenu switchLanguageTo = {switchLanguageTo}/> */}
     <Switch>
           <Route path="/">
-           <Chardonnay/>           {/* <ScreenSwitcher content={[HomeScreen,SecondScreen,PassionMadeVisible,MeetTheCharacter,Generosity,Elegance,FollowTheFox,MeetTheFamily,TerriorScreen,Footer]}/> */}
+           <ScreenSwitcher content={[HomeScreen,SecondScreen,PassionMadeVisible,MeetTheCharacter,Generosity,Elegance,FollowTheFox,MeetTheFamily,TerriorScreen,Footer]}/>
           </Route>
     </Switch>
   </Router>
--- src/Components/Screens/Elegance/elegance.js
+++ src/Components/Screens/Elegance/elegance.js
@@ -1,39 +1,47 @@
-import React from 'react'
+import React , {useState} from 'react'
 import WineBottle from '../../../images/silouett with label.png'
 import LinkComponent from '../../linkComponent/LinkComponent'
 import './elegance.css'
+import VizSensor from 'react-visibility-sensor';
 
 
 
+const MeetTheElegance = () =>{
+    const [visibility,setVisbility]=useState(false);
 
-const MeetTheElegance = () => (
-
-<div className = "elegance-wrapper">
-    <div className="elegance-wrapper__background"></div>
-    <div className="elegance-wrapper__second__background"></div>
-    <div className = "elegance-background">
-        <div className = "elegance-wine-bottle">
-        <img
-            className ="elegance-w-bottle" 
-            src={WineBottle}
-            alt="wineBottle"
-        />
+    return(
+        <VizSensor
+        partialVisibility
+        onChange={(isVisible) => {
+            setVisbility(isVisible)
+        }}
+        >
+        <div className = "elegance-wrapper">
+            <div className="elegance-wrapper__background"></div>
+            <div className={`elegance-wrapper__second__background ${visibility ? 'elegance-wrapper__second__background--animate':''}`}></div>
+            <div className = "elegance-background">
+                <div className = "elegance-wine-bottle">
+                <img
+                    className ="elegance-w-bottle" 
+                    src={WineBottle}
+                    alt="wineBottle"
+                />
+                </div>
+            </div>
+            <div className = "elegance-text">
+                    <h1>Meet The Elegance</h1>
+                    <LinkComponent
+                        name="Go For A Dip"
+                        path="#"
+                        color="#fff"
+                        backgroundColor="#424242"
+                        padding="10px 50px"
+                    />
+            </div>
         </div>
-    </div>
-    <div className = "elegance-text">
-            <h1>Meet The Elegance</h1>
-            <LinkComponent
-                name="Go For A Dip"
-                path="#"
-                color="#fff"
-                backgroundColor="#424242"
-                padding="10px 50px"
-            />
-        </div>
-</div>
+        </VizSensor>
+    )
+}
 
-
-
-);
 
 export default MeetTheElegance;
